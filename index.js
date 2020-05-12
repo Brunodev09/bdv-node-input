80c43fc39c4387350c2e3404b209bc8b327e131e
brunodev <36489087+Brunodev09@users.noreply.github.com>
2020-05-11 21:39:48 -0300

    Adding colors and tags.

--- index.js
+++ index.js
@@ -11,9 +11,63 @@ class BdvNodeInput {
         return this;
     }
 
+    removeColor() {
+        this.color = "\x1b[0m"
+    }
+
+    setTag(tag) {
+        this.tag = tag;
+    }
+
+    setColor(color) {
+        this.color = color;
+        switch (this.color.toUpperCase()) {
+            case 'BRIGHT': this.color = "\x1b[1m";
+                break;
+            case 'DIM': this.color = "\x1b[2m"
+                break;
+            case 'UNDERSCORE': this.color = "\x1b[4m"
+                break;
+            case 'BLINK': this.color = "\x1b[5m"
+                break;
+            case 'FBLACK': this.color = "\x1b[30m"
+                break;
+            case 'FRED': this.color = "\x1b[31m"
+                break;
+            case 'FGREEN': this.color = "\x1b[32m"
+                break;
+            case 'FYELLOW': this.color = "\x1b[33m"
+                break;
+            case 'FBLUE': this.color = "\x1b[34m"
+                break;
+            case 'FMAGENTA': this.color = "\x1b[35m"
+                break;
+            case 'FCYAN': this.color = "\x1b[36m"
+                break;
+            case 'FWHITE': this.color = "\x1b[37m"
+                break;
+            case 'BBLACK': this.color = "\x1b[40m"
+                break;
+            case 'BRRED': this.color = "\x1b[41m"
+                break;
+            case 'BGREEN': this.color = "\x1b[42m"
+                break;
+            case 'BYELLOW': this.color = "\x1b[43m"
+                break;
+            case 'BBLUE': this.color = "\x1b[44m"
+                break;
+            case 'BMAGENTA': this.color = "\x1b[45m"
+                break;
+            case 'BCYAN': this.color = "\x1b[46m"
+                break;
+            case 'BWHITE': this.color = "\x1b[47m"
+                break;
+        }
+    }
+
     question(msg, index) {
         return new Promise((resolve, reject) => {
-            console.log(msg + ":");
+            console.log((this.color || "") + (this.tag || "") + msg + ":");
             msg = "";
             this._rl.question(msg, (answer) => {
                 this.answerList.push(answer);
